f6e1699bec74dff6580e45ab5f6ce4e8652ea73e
Humayun Kayesh <hkayesh@gmail.com>
2018-12-11 15:34:19 +1000

    undo changes to the run_evaluation.sh file

--- src/main/python/run_evaluation.sh
+++ src/main/python/run_evaluation.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#python main_evaluation.py --key cnet_wiki_exp_0; python main_evaluation.py --key cnet_wiki_exp_1; python main_evaluation.py --key cnet_wiki_exp_2; python main_evaluation.py --key cnet_wiki_exp_3; python main_evaluation.py --key cnet_wiki_exp_4; python main_evaluation.py --key cnet_wiki_exp_5;
-#
+python main_evaluation.py --key cnet_wiki_exp_0; python main_evaluation.py --key cnet_wiki_exp_1; python main_evaluation.py --key cnet_wiki_exp_2; python main_evaluation.py --key cnet_wiki_exp_3; python main_evaluation.py --key cnet_wiki_exp_4; python main_evaluation.py --key cnet_wiki_exp_5;
+
 python main_evaluation.py --key cnet_news_exp_0; python main_evaluation.py --key cnet_news_exp_1; python main_evaluation.py --key cnet_news_exp_2; python main_evaluation.py --key cnet_news_exp_3; python main_evaluation.py --key cnet_news_exp_4; python main_evaluation.py --key cnet_news_exp_5;
-#
-#python main_evaluation.py --key luo_threshold_10;
-#
-#python main_evaluation.py --key sasaki_threshold_10;
+
+python main_evaluation.py --key luo_threshold_10;
+
+python main_evaluation.py --key sasaki_threshold_10;
 
 python main_evaluation.py --visualize yes;
